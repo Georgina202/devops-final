008fa4db3274e8f22a5645966447cea88cf28414
Georgina Rivas <rivasgeorgina0@gmail.com>
2025-12-14 22:13:04 -0400

    fix: bind host and port for render v3

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hola Mundo v2 🫡");
+  res.status(200).send("Hola Mundo v3 - test produ. 🫡");
 });
 
 export default app;
